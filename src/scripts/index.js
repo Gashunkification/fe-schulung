209d000785b0a701f6e8e122c85b669192fbd437
jens.primer <jens.primer@arithnea.de>
2017-02-09 21:47:11 +0100

    Initial commit ue3

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,6 @@
-import Autocomplete from 'teleport-autocomplete';
+/* Import von Autocomplete aus node_modules */
 
-const API_KEY = '12455de28945a9185b28127600e08bc8';
-
-
-const getWeatherForCityId = (query) => {
-    if (!query) { return; }
-    const cityId = query.geonameId;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&APPID=' + API_KEY);
-    xhr.addEventListener('load', (event) => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            console.log(xhr.responseText);
-        } else {
-            console.error(xhr.statusText, xhr.responseText);
-        }
-    });
-    xhr.send();
-};
 
 document.addEventListener("DOMContentLoaded", (event) => { 
-    const autoComplete = new Autocomplete({ el: '#location-input', maxItems: 5 });
-    autoComplete.on('change', getWeatherForCityId);
-    console.log(autoComplete);
+    /* Neues Autocomplete */
 });
