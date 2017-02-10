53b90ed156fd90f3a7bcc631ab3fddb2a9ad8efb
jens.primer <jens.primer@arithnea.de>
2017-02-10 14:24:34 +0100

    asd

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,14 +4,11 @@ import Autocomplete from 'teleport-autocomplete';
 /* API Key generieren und hier eintragen */
 const API_KEY = '';
 
+/* Eventlistener bauen, cityID bekommen, wetter laden und anzeigen */
 
-const loadedCallback = (event) => { 
+document.addEventListener("DOMContentLoaded", (event) => { 
     const autoComplete = new Autocomplete({ el: '#location-input', maxItems: 5 });
     autoComplete.on('querychange', (query) => {
         console.log(query);
     });
-}
-
-/* Eventlistener bauen, cityID bekommen, wetter laden und anzeigen */
-
-document.addEventListener("DOMContentLoaded", loadedCallback);
+});
