a2ca832c732ca70e2192295f7b81ecd6362e8e06
jens.primer <jens.primer@arithnea.de>
2017-02-10 14:09:35 +0100

    Einschnelsjdöa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 /* Import von Autocomplete aus node_modules */
+import Autocomplete from 'teleport-autocomplete';
 
-
-document.addEventListener("DOMContentLoaded", (event) => { 
-    /* Neues Autocomplete */
+document.addEventListener("DOMContentLoaded", (event) => {
+    const myAutocomplete = new Autocomplete({ el: '#teleport-input', maxItems: 5 });
 });
