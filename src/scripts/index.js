0617f88c1a1e9ea388f5b7fd16156917264aa0eb
jens.primer <jens.primer@arithnea.de>
2017-02-10 02:32:22 +0100

    Vorbereitung ue4

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,12 @@
 import Autocomplete from 'teleport-autocomplete';
-import CityWeather from './lib/CityWeather';
-import WeatherDisplay from './lib/WeatherDisplay';
+/* Import von Netzwerk und Anzeige Klassen (prebuild) */
 
-const API_KEY = '12455de28945a9185b28127600e08bc8';
+/* API Key generieren und hier eintragen */
+const API_KEY = '';
 
-const getWeatherForCityId = (query) => {
-    if (!query) { return; }
-
-    const cityId = query.geonameId;
-    const cityWeather = new CityWeather(API_KEY);
-
-    cityWeather.get(cityId, (response) => {
-        // Response ist die Antwort der openWeather API
-       const wd = new WeatherDisplay(response);
-       wd.display('weather')
-    });
-};
+/* Eventlistener bauen, cityID bekommen, wetter laden und anzeigen */
 
 document.addEventListener("DOMContentLoaded", (event) => { 
     const autoComplete = new Autocomplete({ el: '#location-input', maxItems: 5 });
-    autoComplete.on('change', getWeatherForCityId);
+    /* event listener an das autocomplete */
 });
