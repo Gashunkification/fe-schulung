d33ffe908bddbf95e007adf45a515e6be5368c09
jens.primer <jens.primer@arithnea.de>
2017-02-16 21:39:46 +0100

    Bla

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,22 @@
 import Autocomplete from 'teleport-autocomplete';
+import CityWeather from './lib/CityWeather';
+import WeatherDisplay from './lib/WeatherDisplay';
 /* Import von Netzwerk und Anzeige Klassen (prebuild) */
 
-/* API Key generieren und hier eintragen */
-const API_KEY = '';
+const API_KEY = '12455de28945a9185b28127600e08bc8';
 
 /* Eventlistener bauen, cityID bekommen, wetter laden und anzeigen */
+const autoCompleteCallback = (query) => {
+    const cityId = query.geonameId;
+    const cityWeather = new CityWeather(API_KEY);
+    
+    cityWeather.get(cityId, (weatherData) => {
+        const weatherDisplay = new WeatherDisplay(weatherData);
+        weatherDisplay.display('weather');
+    });
+}
 
 document.addEventListener("DOMContentLoaded", (event) => { 
     const autoComplete = new Autocomplete({ el: '#location-input', maxItems: 5 });
-    autoComplete.on('querychange', (query) => {
-        console.log(query);
-    });
+    autoComplete.on('change', autoCompleteCallback);
 });
